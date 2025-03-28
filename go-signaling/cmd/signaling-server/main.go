2f9633b2905e2336834f069433343b4b57e01499
SaiSawant1 <saisawant2003@gmail.com>
2025-03-28 22:20:39 +0530

    file cleanup

--- go-signaling/cmd/signaling-server/main.go
+++ go-signaling/cmd/signaling-server/main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/SaiSawant1/p2p/signaling/internal/routes"
 )
 
 func main() {
 	fmt.Println("hello world!")
-	routes.SomeRoute()
 }
--- go-signaling/internal/routes/routes.go
+++
@@ -1,7 +0,0 @@
-package routes
-
-import "fmt"
-
-func SomeRoute() {
-	fmt.Println("SomeRoute")
-}
